0af3f2296bdab32c98cbcc42daf6127cd6b28c23
NNam2411 <nguyennam2002ro@gmail.com>
2023-10-11 15:29:21 +0700

    Finish

---
+++ 20110383/back_end/models/post.js
@@ -0,0 +1,23 @@
+const listpost = [
+  {
+    id: 1,
+    content: "Hello la NNAM",
+    author: "Nhat Nam",
+    listcomment: [
+      { id: 1, content: "Hellossss" },
+      { id: 2, content: "Hisssas" },
+    ],
+  },
+  {
+    id: 2,
+    content: "Hello la GKhanh",
+    author: "Khanh",
+    listcomment: [
+      { id: 1, content: "Hellosnnnnn" },
+      { id: 2, content: "Hihihi" },
+    ],
+  },
+];
+module.exports = {
+  listpost,
+};
